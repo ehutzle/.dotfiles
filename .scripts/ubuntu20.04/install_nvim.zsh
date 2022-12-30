64a0ca5d168cdc5db424a75ab0607b1d359263c6
Eric Richard <ehutzle@gmail.com>
2022-12-30 18:06:52 -0500

    Updated nvim install script

--- .scripts/ubuntu20.04/install_nvim.zsh
+++ .scripts/ubuntu20.04/install_nvim.zsh
@@ -1,5 +1,11 @@
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:neovim-ppa/stable
-sudo apt-get update
-sudo apt-get install neovim
+sudo apt-get install ninja-build \
+	gettext libtool libtool-bin \
+	autoconf automake cmake g++ \
+	pkg-config unzip
 
+git clone https://github.com/neovim/neovim.git
+cd neovim
+git checkout stable
+sudo make CMAKE_BUILD_TYPE=Release install
+cd ..
+rm -rf neovim
