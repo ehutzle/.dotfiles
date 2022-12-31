5236d9aec2b0a0f7ea0fd593050d5e1311b2b2a3
ehutzle <35176511+ehutzle@users.noreply.github.com>
2022-12-30 23:34:13 -0500

    fugitive keybindings

--- .config/nvim/after/plugin/fugitive.lua
+++ .config/nvim/after/plugin/fugitive.lua
@@ -1,3 +1,9 @@
 -- Fugitive
+vim.keymap.set('n', '<leader>gp', function ()
+    vim.cmd('Git push')
+end)
+vim.keymap.set('n', '<leader>gg', function ()
+    vim.cmd('Git pull')
+end)
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -5,3 +5,6 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+vim.keymap.set('n', '<leader>pp', function ()
+    vim.cmd('Git push')
+end)
