6327cec20a3b1037e0d5ffc07a068c215b9fda52
Jim <ehutzle@gmail.com>
2023-01-03 05:20:20 -0500

    added simpylfold config

---
+++ .config/nvim/after/plugin/simpylfold.lua
@@ -0,0 +1,11 @@
+-- https://github.com/tmhedberg/SimpylFold
+
+vim.g.SimpylFold_docstring_preview = 1
+vim.g.SimpylFold_python_fold_docstrings = 0
+
+-- fold -> `zc`
+-- unfold -> `zo`
+-- toggle -> `za`
+-- unfold all -> `zR`
+-- fold all -> `zM`
+-- fold level -> `z1` to `z9`
