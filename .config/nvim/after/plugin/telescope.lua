9d60dff5524a789f79269bb32efd72251cef48c7
Eric <ehutzle@gmail.com>
2023-01-02 19:27:27 -0500

    updating kitty.conf and added kitty.conf syntax highlighting to packer removed "<leader>ps" grep (not working)

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -2,9 +2,6 @@ local builtin = require('telescope.builtin')
     -- `false` will disable the whole extension
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
 vim.keymap.set('n', '<leader>pp', function ()
     vim.cmd('Git push')
 end)
--- .config/nvim/lua/jimeagle/packer.lua
+++ .config/nvim/lua/jimeagle/packer.lua
@@ -81,4 +81,5 @@ return require('packer').startup(function(use)
         end
     }
     use('github/copilot.vim')
+    use('fladson/vim-kitty')
 end)
